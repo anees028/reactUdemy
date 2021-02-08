4d5662ffea00c5f4c04bc5da47f42aa4ac5bd7b9
anees028 <rehmananees028@gmail.com>
2021-02-08 21:31:19 +0500

    First Git Upload

--- src/components/news_list_item.js
+++ src/components/news_list_item.js
@@ -3,24 +3,25 @@ import { css } from 'glamor';
 
 const NewsItem = ({item}) => {
 
-    let news_item = css({
-        padding: '20px',
-        boxSizing: 'border-box',
-        borderBottom:'2px solid grey',
-        borderRadius:'20px',
-        marginTop:'5px',
-        ':hover':{
-            color:'white',
-            background:'blue'
-        }
-    })
+    // let news_item = css({
+    //     padding: '20px',
+    //     boxSizing: 'border-box',
+    //     borderBottom:'2px solid grey',
+    //     borderRadius:'20px',
+    //     marginTop:'5px',
+    //     ':hover':{
+    //         color:'white',
+    //         background:'blue'
+    //     }
+    // })
 
-    let item_grey = css ({
-        background:'lightgrey'
-    })
+    // let item_grey = css ({
+    //     background:'lightgrey'
+    // })
 
     return(
-        <div className={`${news_item} ${item_grey}`}>
+        // <div className={`${news_item} ${item_grey}`}>   //For changing color during hover.....
+        <div className="news_item" >
             <h2>{item.title}</h2>
             <div>
                 {item.feed}
